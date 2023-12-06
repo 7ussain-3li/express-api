39df67081983e81f7c8e639adbc26b8f31302386
7ussain-3li <105246805+7ussain-3li@users.noreply.github.com>
2023-12-06 23:48:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+app.use(express.json());
+
+const data = fs.readFileSync("./users.json", "utf8");
+const users = JSON.parse(data);
+
+app.get('/', (req, res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
